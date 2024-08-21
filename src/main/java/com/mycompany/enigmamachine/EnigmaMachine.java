4f94e10c8081a99134306b8123ed72ec09315b5b
Antoni Nowaczyk <antoninowaczyk1@gmail.com>
2024-08-22 01:34:56 +0200

    Added two classes.

---
+++ src/main/java/com/mycompany/enigmamachine/EnigmaMachine.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.enigmamachine;
+
+/**
+ *
+ * @author 20231367
+ */
+public class EnigmaMachine {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/enigmamachine/Rotor.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.enigmamachine;
+
+/**
+ *
+ * @author 20231367
+ */
+public class Rotor {
+    
+}
